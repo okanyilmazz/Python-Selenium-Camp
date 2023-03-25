91ee53b8e3b1d2a2b88901f336c34cbfec4e237d
Okan YILMAZ <okan_yilmaz28@hotmail.com>
2023-03-26 00:58:14 +0300

    homework completed

--- home-work-week-4/test_sauce_home_work.py
+++ home-work-week-4/test_sauce_home_work.py
@@ -1,3 +1,4 @@
+from datetime import date
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from time import sleep
@@ -5,71 +6,85 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
+import pytest
+from pathlib import Path
 
 
 class Test_Sauce_Work:
-    driver = webdriver.Chrome(ChromeDriverManager().install())
-    driver.maximize_window()
-    driver.get("https://www.saucedemo.com/")
-    sleep(2)
+    def setup_method(self):
+        self.driver = webdriver.Chrome(ChromeDriverManager().install())
+        self.driver.maximize_window()
+        self.driver.get("https://www.saucedemo.com/")
+        self.folderPath = str(date.today())
+        Path(self.folderPath).mkdir(exist_ok=True)
 
+    def teardown_method(self):
+        self.driver.quit()
 
     def test_null_login(self):
+        self.waitForElementVisible((By.ID, 'login-button'))
         loginBtn = self.driver.find_element(By.ID, "login-button")
-        sleep(2)
         loginBtn.click()
         errorMessage = self.driver.find_element(
             By.XPATH, "//*[@id='login_button_container']/div/form/div[3]/h3")
-        testResult = errorMessage.text == "Epic sadface: Username is required"
-        print(f"Null Login Test: {testResult}")
+        self.driver.save_screenshot(self.folderPath+"/test-null-login.png")
+        assert errorMessage.text == "Epic sadface: Username is required"
 
     def test_null_password(self):
+        self.waitForElementVisible((By.ID, 'user-name'))
         usernameInput = self.driver.find_element(By.ID, "user-name")
-        sleep(1)
         usernameInput.send_keys("1")
+        self.waitForElementVisible((By.ID, 'login-button'))
         loginBtn = self.driver.find_element(By.ID, "login-button")
-        sleep(2)
         loginBtn.click()
         errorMessage = self.driver.find_element(
             By.XPATH, "//*[@id='login_button_container']/div/form/div[3]/h3")
-        testResult = errorMessage.text == "Epic sadface: Password is required"
-        print(f"Null Password Test: {testResult}")
+        self.driver.save_screenshot(self.folderPath+"/test-null-password.png")
+        assert errorMessage.text == "Epic sadface: Password is required"
 
     def test_locked_user(self):
+        self.waitForElementVisible((By.ID, 'user-name'))
         usernameInput = self.driver.find_element(By.ID, "user-name")
+        self.waitForElementVisible((By.ID, 'password'))
         passwordInput = self.driver.find_element(By.ID, "password")
-        sleep(5)
+
         usernameInput.send_keys("locked_out_user")
         passwordInput.send_keys("secret_sauce")
+        self.waitForElementVisible((By.ID, 'login-button'))
         loginBtn = self.driver.find_element(By.ID, "login-button")
-        sleep(2)
         loginBtn.click()
         errorMessage = self.driver.find_element(
             By.XPATH, "//*[@id='login_button_container']/div/form/div[3]/h3")
-        testResult = errorMessage.text == "Epic sadface: Sorry, this user has been locked out."
-        print(f"Locked User Test: {testResult}")
+        self.driver.save_screenshot(self.folderPath+"/test-locked_user.png")
+        assert errorMessage.text == "Epic sadface: Sorry, this user has been locked out."
 
-    def show_error_icon_test(self):
+    def test_show_error_icon_test(self):
+        self.waitForElementVisible((By.ID, 'login-button'))
         loginBtn = self.driver.find_element(By.ID, "login-button")
-        sleep(2)
         loginBtn.click()
-        sleep(2)
+        self.waitForElementVisible((By.CLASS_NAME, 'error_icon'))
         errorIcons = self.driver.find_elements(By.CLASS_NAME, "error_icon")
+
         if (errorIcons.__len__() > 1):
-            print(f"Error Icon Test : {True}")
+            self.driver.save_screenshot(
+                self.folderPath+"/test_show_error_icon.png")
+            assert True
         else:
-            print(f"Error Icon Test : {False}")
+            assert False
+
+        self.waitForElementVisible((By.CLASS_NAME, 'error-button'))
         closeButton = self.driver.find_element(By.CLASS_NAME, "error-button")
-        sleep(1)
         closeButton.click()
-        sleep(1)
 
         # 1. Yöntem
         errorIcons = self.driver.find_elements(By.CLASS_NAME, "error_icon")
         if (errorIcons.__len__() > 1):
-            print(f"Error icon not appearing after clicking X Button: {False}")
+
+            assert False
         else:
-            print(f"Error icon not appearing after clicking X Button: {True}")
+            self.driver.save_screenshot(
+                self.folderPath+"/test_close_error_icon.png")
+            assert True
 
         # 2. Yöntem
         # try: self.driver.find_element(By.CLASS_NAME,"error_icon")
@@ -77,40 +92,105 @@ class Test_Sauce_Work:
         #     return print(f"Error icon not appearing after clicking X Button: {True}")
         # return False
 
-    def login_standard_user(self):
+    def test_login_standard_user(self):
+        self.waitForElementVisible((By.ID, 'user-name'))
         usernameInput = self.driver.find_element(By.ID, "user-name")
+        self.waitForElementVisible((By.ID, 'password'))
         passwordInput = self.driver.find_element(By.ID, "password")
-        sleep(1)
+
         usernameInput.send_keys("standard_user")
         passwordInput.send_keys("secret_sauce")
+
+        self.waitForElementVisible((By.ID, 'login-button'))
         loginBtn = self.driver.find_element(By.ID, "login-button")
-        sleep(2)
         loginBtn.click()
-        sleep(2)
+
         get_url = self.driver.current_url
         if (get_url == "https://www.saucedemo.com/inventory.html"):
-            print(f"is the current url equal to /inventory.html : {True}")
+            self.driver.save_screenshot(
+                self.folderPath+"/test_login_standard_user.png")
+            assert True
         else:
-            print(False)
+            assert False
 
-    def show_product_count(self):
-        self.login_standard_user()
-        sleep(2)
+    def test_show_product_count(self):
+        self.test_login_standard_user()
+        self.waitForElementVisible((By.CLASS_NAME, 'inventory_item'))
         productList = self.driver.find_elements(
             By.CLASS_NAME, "inventory_item")
         if (productList.__len__() == 6):
-            print(f"Product count equal to 6 : {True}")
+            self.driver.save_screenshot(
+                self.folderPath+"/test_show_product_count.png")
+            assert True
         else:
-            print(f"Product count equal to 6 : {False}")
- 
+            assert False
 
-testClass = Test_Sauce_Work()
-# testClass.test_null_login()
-# testClass.test_null_password()
-# testClass.test_locked_user()
-# testClass.show_error_icon_test()
-# testClass.login_standard_user()
-testClass.show_product_count()
+    def test_invalid_login(self):
+        self.waitForElementVisible((By.ID, 'user-name'))
+        usernameInput = self.driver.find_element(By.ID, "user-name")
+        self.waitForElementVisible((By.ID, 'password'))
+        passwordInput = self.driver.find_element(By.ID, "password")
+        usernameInput.send_keys("1")
+        passwordInput.send_keys("1")
+        self.waitForElementVisible((By.ID, 'login-button'))
+        loginBtn = self.driver.find_element(By.ID, "login-button")
+        loginBtn.click()
+        errorMessage = self.driver.find_element(
+            By.XPATH, "//*[@id='login_button_container']/div/form/div[3]/h3")
+        self.driver.save_screenshot(self.folderPath+"/test_invalid_login.png")
+        assert errorMessage.text == "Epic sadface: Username and password do not match any user in this service"
 
-while True:
-    continue
+    def test_add_all_items_to_cart(self):
+        self.test_login_standard_user()
+        self.waitForElementVisible((By.CLASS_NAME, 'btn_inventory'))
+        inventoryBtnList = self.driver.find_elements(
+            By.CLASS_NAME, "btn_inventory")
+        for inventoryBtn in inventoryBtnList:
+            inventoryBtn.click()
+
+        self.waitForElementVisible((By.CLASS_NAME, 'shopping_cart_container'))
+        cartBtn = self.driver.find_element(
+            By.CLASS_NAME, "shopping_cart_container")
+        cartBtn.click()
+
+        self.waitForElementVisible((By.CLASS_NAME, 'cart_item'))
+        itemList = self.driver.find_elements(By.CLASS_NAME, "cart_item")
+
+        if (itemList.__len__() == 6):
+            self.driver.save_screenshot(
+                self.folderPath+"/test_add_all_items_to_cart.png")
+            assert True
+        else:
+            assert False
+
+    def test_confirm_order(self):
+        self.test_add_all_items_to_cart()
+        self.waitForElementVisible((By.ID, 'checkout'))
+        checkoutBtn = self.driver.find_element(By.ID, 'checkout')
+        checkoutBtn.click()
+
+        self.waitForElementVisible((By.ID, 'first-name'))
+        firstNameInput = self.driver.find_element(By.ID, "first-name")
+        self.waitForElementVisible((By.ID, 'last-name'))
+        lastNameInput = self.driver.find_element(By.ID, "last-name")
+        self.waitForElementVisible((By.ID, 'postal-code'))
+        postalCodeInput = self.driver.find_element(By.ID, "postal-code")
+
+        firstNameInput.send_keys("Okan")
+        lastNameInput.send_keys("Yılmaz")
+        postalCodeInput.send_keys("0000")
+        self.waitForElementVisible((By.ID, 'continue'))
+        continueBtn = self.driver.find_element(By.ID, "continue")
+        continueBtn.click()
+        finishBtn = self.driver.find_element(By.ID, "finish")
+        finishBtn.click()
+
+        self.waitForElementVisible((By.CLASS_NAME, 'complete-text'))
+        successMessage = self.driver.find_element(
+            By.CLASS_NAME, 'complete-text')
+        self.driver.save_screenshot(self.folderPath+"/test_confirm_order.png")
+        assert successMessage.text == "Your order has been dispatched, and will arrive just as fast as the pony can get there!"
+
+    def waitForElementVisible(self, locator, timeout=5):
+        WebDriverWait(self.driver, timeout).until(
+            ec.visibility_of_element_located(locator))
